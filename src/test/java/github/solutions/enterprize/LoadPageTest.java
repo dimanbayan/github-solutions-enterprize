f047d28f1449f56e573892fdf4835bc39bf3448c
dmitry mumolin <dmitrbayan5@yandex.ru>
2023-08-27 09:34:28 +0300

    first commit

---
+++ src/test/java/github/solutions/enterprize/LoadPageTest.java
@@ -0,0 +1,21 @@
+package github.solutions.enterprize;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class LoadPageTest {
+
+    @Test
+    public void LoadingPage(){
+        open("https://github.com/");
+        $(byTagAndText("button", "Solutions")).hover();
+        $(byTagAndText("button", "Solutions")).sibling(0).$(byText("Enterprise")).click();
+        $("div.enterprise-hero h1").shouldHave(text("Build like the best"));
+    }
+}
